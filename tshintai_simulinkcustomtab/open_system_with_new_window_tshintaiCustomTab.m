e465b820fa176dfcb258b49e2fe71833c187ce97
tshintai <tshintai@mathworks.com>
2022-03-11 13:35:14 +0900

    update to R2022a.

--- tshintai_simulinkcustomtab/open_system_with_new_window_tshintaiCustomTab.m
+++ tshintai_simulinkcustomtab/open_system_with_new_window_tshintaiCustomTab.m
@@ -27,14 +27,14 @@ for i = 1:numel(selected_block_list)
 
     if strcmp(selected_block_list{i}, model_root)
 
-        open_system(selected_block_list{i});
+        open_system_near_bdroot(selected_block_list{i}, model_root);
 
     else
 
         if strcmp(get_param(selected_block_list{i}, 'BlockType'), ...
                 'ModelReference')
             ref_model_name = get_param(selected_block_list{i}, 'ModelName');
-            open_system(ref_model_name);
+            open_system_near_bdroot(ref_model_name, model_root);
 
         else
 
@@ -46,13 +46,87 @@ for i = 1:numel(selected_block_list)
             end
 
             if ~isempty(subsystem_ref_name)
-                open_system(subsystem_ref_name);
+                open_system_near_bdroot(subsystem_ref_name, model_root);
             else
-                open_system(selected_block_list{i}, 'window');
+                open_system_near_bdroot(selected_block_list{i}, model_root);
             end
 
         end
     end
 end
+
+end
+
+function open_system_near_bdroot(model_name, model_root)
+%%
+open_system(model_name, 'window');
+
+if strcmp(model_root, model_name)
+    return;
+end
+
+%%
+location_offset = 30;
+root_location = get_param(model_root, 'Location');
+model_location = get_param(model_name, 'Location');
+
+if ~isempty(root_location(root_location <= 0))
+    % 位置情報にマイナス値がある場合、
+    % ウィンドウ位置を正しく動かせないのでここで中断する。
+    return;
+end
+
+%%
+starting_point = root_location(1:2) + location_offset;
+
+next_position = [...
+    starting_point(1), ...
+    starting_point(2), ...
+    starting_point(1) + (model_location(3) - model_location(1)), ...
+    starting_point(2) + (model_location(4) - model_location(2)) ];
+
+%%
+set_param(model_name, 'Location', next_position);
+
+%% 
+% ここからの処理はリスクがあるため、デフォルトでは無効化している。
+% 問題無いようであれば、以下のreturnを削除して有効化すること。
+return;
+%%
+Tab_offset = [480, 40];
+graphic_obj = groot;
+origin_position = graphic_obj.PointerLocation;
+
+%%
+pointer_segment = 1;
+Monitor_positions = graphic_obj.MonitorPositions;
+for i = 1:size(Monitor_positions, 1)
+    if (starting_point(1) >= Monitor_positions(i, 1) && ...
+        starting_point(1) <= (Monitor_positions(i, 1) - 1 + Monitor_positions(i, 3)) && ...
+        starting_point(2) >= Monitor_positions(i, 2) && ...
+        starting_point(2) <= (Monitor_positions(i, 2) - 1 + Monitor_positions(i, 4)) )
+            pointer_segment = i;
+    end
+end
+
+vertical_offset = Monitor_positions(pointer_segment, 2) ...
+    - 1 + Monitor_positions(pointer_segment, 4);
+
+%%
+pointer_position = [...
+    next_position(1) + Tab_offset(1), ...
+    vertical_offset - next_position(2) - Tab_offset(2), ...
+    ];
+
+graphic_obj.PointerLocation = pointer_position;
+
+%% Javaの自動クリック機能を使用
+import java.awt.Robot;
+import java.awt.event.*;
+mouse = Robot;
+mouse.mousePress(InputEvent.BUTTON1_MASK);
+mouse.mouseRelease(InputEvent.BUTTON1_MASK);
+
+graphic_obj.PointerLocation = origin_position;
 
 end
--- tshintai_simulinkcustomtab/root_run_tshintaiCustomTab.m
+++ tshintai_simulinkcustomtab/root_run_tshintaiCustomTab.m
@@ -5,6 +5,13 @@ function root_run_tshintaiCustomTab()
 % 上位階層のモデルが閉じられている場合は、検索されません。
 %%
 child_model_name = bdroot;
+if strcmp(get_param(child_model_name, "IsHarness"), 'on')
+    % テストハーネスモデルの場合、そのモデルを実行
+    set_param(child_model_name, 'SimulationCommand', 'start');
+    return;
+else
+    child_model_info = Simulink.MDLInfo(child_model_name);
+end
 
 loaded_models = Simulink.allBlockDiagrams('model');
 loaded_models_name = get_param(loaded_models,'Name');
@@ -13,25 +20,30 @@ other_models_name = loaded_models_name(~strcmp(loaded_models_name, ...
     child_model_name));
 
 if (numel(other_models_name) < 1)
-    set_param(child_model_name, 'SimulationCommand', 'start');
+    if strcmp(child_model_info.BlockDiagramType, 'Model')
+        set_param(child_model_name, 'SimulationCommand', 'start');
+    end
     return;
 end
 
 %%
-related = false(numel(other_models_name), 1);
-for i = 1:numel(other_models_name)
+if strcmp(child_model_info.BlockDiagramType, 'Model')
+    related_model_name = find_model_referencing(...
+        other_models_name, child_model_name);
 
-    [ref_model_name, ~] = find_mdlrefs(other_models_name{i});
-    find_name = ref_model_name(strcmp(ref_model_name, child_model_name));
-
-    if ~isempty(find_name)
-        related(i) = true;
+    if (numel(related_model_name) < 1)
+        set_param(child_model_name, 'SimulationCommand', 'start');
+        return;
     end
-end
 
-related_model_name = other_models_name(related);
-if (numel(related_model_name) < 1)
-    set_param(child_model_name, 'SimulationCommand', 'start');
+elseif strcmp(child_model_info.BlockDiagramType, 'Subsystem')
+    related_model_name = find_subsystem_referencing(...
+        child_model_name);
+
+    if (numel(related_model_name) < 1)
+        return;
+    end
+else
     return;
 end
 
@@ -73,6 +85,60 @@ else
     set_param(related_model_name{root_model_index(1)}, ...
         'SimulationCommand', 'start');
 
+end
+
+end
+
+
+function related_model_name = find_model_referencing(...
+    other_models_name, child_model_name)
+
+related = false(numel(other_models_name), 1);
+for i = 1:numel(other_models_name)
+
+    [ref_model_name, ~] = find_mdlrefs(other_models_name{i});
+    find_name = ref_model_name(strcmp(ref_model_name, child_model_name));
+
+    if ~isempty(find_name)
+        related(i) = true;
+    end
+end
+
+related_model_name = other_models_name(related);
+
+end
+
+
+function related_model_name = find_subsystem_referencing(...
+    child_model_name)
+related_model_name = [];
+
+if strcmp(version('-release'), '2021b')
+    % Do Nothing.
+else
+    model_name = ...
+        Simulink.SubsystemReference.getActiveInstances(...
+        child_model_name);
+
+    if numel(model_name) < 1.5
+        return;
+    end
+
+    related_model_name = cell(numel(model_name) - 1, 1);
+    valid_model_flag = true(numel(related_model_name), 1);
+    for i = 2:numel(model_name)
+        temp = strsplit(model_name{i}, '/');
+        related_model_name{i - 1} = temp{1};
+
+        info = Simulink.MDLInfo(temp{1});
+        if ~strcmp(info.BlockDiagramType, 'Model')
+            valid_model_flag(i - 1) = false;
+        end
+    end
+
+    % サブシステムと被っているモデル名を外す
+    related_model_name = related_model_name(valid_model_flag);
+    related_model_name = unique(related_model_name);
 end
 
 end
