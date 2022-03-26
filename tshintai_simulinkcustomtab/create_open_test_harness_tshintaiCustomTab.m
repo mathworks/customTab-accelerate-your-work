ed232b8368e957bcc9b8fa08a3708dcd20201980
tshintai <tshintai@mathworks.com>
2022-03-26 11:40:47 +0900

    revise create harness script.

--- tshintai_simulinkcustomtab/create_open_test_harness_tshintaiCustomTab.m
+++ tshintai_simulinkcustomtab/create_open_test_harness_tshintaiCustomTab.m
@@ -46,8 +46,8 @@ elseif numel(selected_block_list) > 1
     return;
 else
 
-    subsystem_flag = check_block_is_subsystem(selected_block_list{1});
-    if (subsystem_flag(1) == 1)
+    subsystem_type = check_block_is_subsystem(selected_block_list{1});
+    if (subsystem_type(1) == 1)
         % 通常のサブシステムの場合
         harness_list = sltest.harness.find(selected_block_list{1});
 
@@ -56,7 +56,7 @@ else
             block_name = get_param(selected_block_list{1}, 'Name');
             harness_name = replace_bad_names([block_name, '_harness']);
             create_open_new_harness(model_name, selected_block_list{1}, ...
-                harness_name{1}, subsystem_flag);
+                harness_name{1}, subsystem_type);
 
         elseif numel(harness_list) == 1
 
@@ -67,7 +67,7 @@ else
             choose_from_multiple_harnesses(harness_list, selected_block_list{1});
 
         end
-    elseif (subsystem_flag(1) == 2)
+    elseif (subsystem_type(1) == 2)
         % 参照サブシステムの場合
         subsystem_ref_name = get_param(selected_block_list{1}, ...
             'ReferencedSubsystem');
@@ -78,7 +78,7 @@ else
 
             harness_name = replace_bad_names([subsystem_ref_name, '_harness']);
             create_open_new_harness(model_name, subsystem_ref_name, ...
-                harness_name{1}, subsystem_flag);
+                harness_name{1}, subsystem_type);
 
         else
 
@@ -92,7 +92,7 @@ else
 
         end
 
-    elseif (subsystem_flag(1) == 3)
+    elseif (subsystem_type(1) == 3)
         % 参照モデルの場合
         ref_model_name = get_param(selected_block_list{1}, 'ModelName');
         load_system(ref_model_name);
@@ -102,7 +102,7 @@ else
 
             harness_name = replace_bad_names([ref_model_name, '_harness']);
             create_open_new_harness(ref_model_name, ref_model_name, ...
-                harness_name{1}, subsystem_flag);
+                harness_name{1}, subsystem_type);
 
         else
 
@@ -116,6 +116,33 @@ else
 
         end
 
+    else
+        % 通常のブロックの場合、
+        % 参照サブシステムを作成してからテストハーネスを作成する
+        block_handle = get_param(selected_block_list{1}, 'Handle');
+        parent_name  = get_param(selected_block_list{1}, 'Parent');
+        block_name   = replace_bad_names( ...
+            get_param(selected_block_list{1}, 'Name'));
+        subsystem_name = [block_name{1}, '__s'];
+        subsystem_path = [parent_name, '/', subsystem_name];
+        block_pos = get_param(selected_block_list{1}, 'Position');
+
+        % 削除する時のため、ブロック名は修正する
+        set_param(selected_block_list{1}, 'Name', block_name{1});
+
+        Simulink.BlockDiagram.createSubsystem(block_handle, ...
+            'Name', subsystem_name);
+        set_param(subsystem_path, 'Position', block_pos);
+
+        if exist([subsystem_name, '.slx'], 'file')
+            delete([subsystem_name, '.slx']);
+        end
+        Simulink.SubsystemReference.convertSubsystemToSubsystemReference( ...
+            subsystem_path, subsystem_name);
+
+        harness_name = replace_bad_names([subsystem_name, '_harness']);
+        create_open_new_harness(model_name, subsystem_name, ...
+            harness_name{1}, 2);
     end
 end
 
@@ -144,33 +171,9 @@ end
 function create_open_new_harness(model_name, system_path, harness_name, ...
     subsystem_type)
 %%
-try
-    sltest.harness.create(system_path, 'Name', harness_name, ...
-        'CreateWithoutCompile', true);
-    sltest.harness.open(system_path, harness_name);
-
-catch
-    % エラーの場合は、そのブロックに対してサブシステムを作ってから
-    % ハーネスを作る。
-    block_handle = get_param(system_path, 'Handle');
-    parent_name  = get_param(system_path, 'Parent');
-    block_name   = replace_bad_names( ...
-        get_param(system_path, 'Name'));
-    subsystem_name = [block_name{1}, '__s'];
-    subsystem_path = [parent_name, '/', subsystem_name];
-    block_pos = get_param(system_path, 'Position');
-
-    % 削除する時のため、ブロック名は修正する
-    set_param(system_path, 'Name', block_name{1});
-
-    Simulink.BlockDiagram.createSubsystem(block_handle, ...
-        'Name', subsystem_name);
-    set_param(subsystem_path, 'Position', block_pos);
-
-    sltest.harness.create(subsystem_path, 'Name', harness_name, ...
-        'CreateWithoutCompile', true);
-    sltest.harness.open(subsystem_path, harness_name);
-end
+sltest.harness.create(system_path, 'Name', harness_name, ...
+    'CreateWithoutCompile', true);
+sltest.harness.open(system_path, harness_name);
 
 %%
 BW_access = get_param(model_name, 'EnableAccessToBaseWorkspace');
@@ -186,10 +189,6 @@ set_param(harness_name, 'ShowLineDimensions', 'on');
 inport_list = find_system(harness_name, ...
     'SearchDepth',1, ...
     'regexp', 'on', 'blocktype', 'Inport');
-
-if (numel(inport_list) < 1)
-    return;
-end
 
 for i = 1:numel(inport_list)
     inport_name = get_param(inport_list{i}, 'Name');
@@ -210,6 +209,42 @@ for i = 1:numel(inport_list)
     Simulink.sdi.markSignalForStreaming(line_handle, 'on');
 end
 
+%%
+outport_list = find_system(harness_name, ...
+    'SearchDepth',1, ...
+    'regexp', 'on', 'blocktype', 'Outport');
+
+for i = 1:numel(outport_list)
+    outport_name = get_param(outport_list{i}, 'Name');
+    outport_pos  = get_param(outport_list{i}, 'Position');
+    port_handle  = get_param(outport_list{i}, 'PortHandles');
+    line_handle  = get_param(port_handle.Inport, 'Line');
+    set_param(line_handle, 'Name', outport_name);
+
+    disp_block_name = [harness_name, '/Display_', num2str(i)];
+    add_block('simulink/Sinks/Display', ...
+        disp_block_name);
+    
+    disp_block_pos = get_param(disp_block_name, 'Position');
+    pos_distance_Y = ...
+        (outport_pos(2) + outport_pos(4) - ...
+        disp_block_pos(2) - disp_block_pos(4)) / 2;
+    desired_pos = [...
+        outport_pos(1), ...
+        disp_block_pos(2) + pos_distance_Y, ...
+        disp_block_pos(3) + outport_pos(1) - disp_block_pos(1), ...
+        disp_block_pos(4) + pos_distance_Y ];
+
+    delete_block(outport_list{i});
+
+    set_param(disp_block_name, 'Position', desired_pos);
+
+    Simulink.sdi.markSignalForStreaming(line_handle, 'on');
+
+    set_param(disp_block_name, 'Commented', 'on');
+end
+
+%%
 if (subsystem_type == 2)
     linked_sldd_name = get_param(model_name, 'DataDictionary');
     if ~isempty(linked_sldd_name)
@@ -250,7 +285,14 @@ for i = 1:numel(block_list)
         end
 
         if isempty(subsystem_ref_name)
-            subsystem_type(i) = 1;
+            if strcmp( ...
+                    get_param(block_list{i}, 'StaticLinkStatus'), ...
+                    'resolved')
+                % ライブラリリンクされたブロックは通常のブロック扱いとする
+                subsystem_type(i) = 0;
+            else
+                subsystem_type(i) = 1;
+            end
         else
             subsystem_type(i) = 2;
         end
