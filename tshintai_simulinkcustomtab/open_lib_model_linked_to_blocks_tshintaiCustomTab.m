ed1ee6c93d679081878f9e3dfa6f88e384b5991a
tshintai <tshintai@mathworks.com>
2022-04-24 16:30:05 +0900

    revise open lib function.

--- tshintai_simulinkcustomtab/open_lib_model_linked_to_blocks_tshintaiCustomTab.m
+++ tshintai_simulinkcustomtab/open_lib_model_linked_to_blocks_tshintaiCustomTab.m
@@ -14,7 +14,7 @@ end
 
 %%
 for i = 1:list_num
-    linked_block_path = get_param(selected_block_list{i}, 'ReferenceBlock');
+    linked_block_path = get_linked_block_path(selected_block_list{i});
     if ~isempty(linked_block_path)
         path_text = strsplit(linked_block_path, '/');
         open_system(path_text{1});
@@ -22,6 +22,19 @@ for i = 1:list_num
         lib_block_parent_path = get_param(lib_block_path, 'Parent');
         open_system(lib_block_parent_path);
     end
+end
+
+end
+
+function linked_path = get_linked_block_path(block_path)
+
+reference_path = get_param(block_path, 'ReferenceBlock');
+ancestor_path = get_param(block_path, 'AncestorBlock');
+
+if isempty(reference_path)
+    linked_path = ancestor_path;
+else
+    linked_path = reference_path;
 end
 
 end
