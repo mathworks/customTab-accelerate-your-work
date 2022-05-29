15bd5456697d7c0ba6a799242b57f210d44c38ff
tshintai <tshintai@mathworks.com>
2022-05-29 13:46:31 +0900

    Revised open config function.

--- tshintai_simulinkcustomtab/open_config_tshintaiCustomTab.m
+++ tshintai_simulinkcustomtab/open_config_tshintaiCustomTab.m
@@ -1,13 +1,44 @@
 function open_config_tshintaiCustomTab()
 %% 説明
-% モデルのコンフィギュレーションパラメーターを開く。
-% そのモデルのコンフィギュレーションパラメーターが参照の場合は、
+% モデルの今開いている階層のコンフィギュレーションパラメーターを開く。
+% 例えば、上位階層のモデルから参照モデルを開いている時、
+% その参照先のモデルのコンフィギュレーションパラメーターを開く。
+% そのモデルがコンフィギュレーション参照を行っている場合は、
 % 参照先のコンフィギュレーションパラメーターを開く。
 %%
-model_name = bdroot;
-activeConfigObj = getActiveConfigSet(model_name);
+top_model_name = bdroot;
+this_model_name = gcs;
+
+if strcmp(top_model_name, this_model_name)
+    activeConfigObj = getActiveConfigSet(top_model_name);
+else
+    try
+        subsystem_ref_name = get_param( ...
+            this_model_name, 'ReferencedSubsystem');
+    catch
+        subsystem_ref_name = '';
+    end
+
+    if (exist(this_model_name, 'file') == 4)
+        % 参照モデルである
+        activeConfigObj = getActiveConfigSet(this_model_name);
+    elseif ~isempty(subsystem_ref_name)
+        % 参照サブシステムである
+        activeConfigObj = getActiveConfigSet(subsystem_ref_name);
+    else
+        activeConfigObj = getActiveConfigSet(top_model_name);
+    end
+end
+
 
 %%
+open_config(activeConfigObj)
+
+end
+
+
+function open_config(activeConfigObj)
+
 if isa(activeConfigObj, 'Simulink.ConfigSet')
     activeConfigObj.openDialog;
 elseif isa(activeConfigObj, 'Simulink.ConfigSetRef')
